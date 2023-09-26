52f2baeab912d399078d4bd87e54bf66c277b889
daianavieira821 <144158523+daianavieira821@users.noreply.github.com>
2023-09-26 14:01:15 -0300

    Prova daiana e kayki
    Prova

---
+++ Prova.py
@@ -0,0 +1,3 @@
+numero = int(input("Digite um número:"))#Aqui fica a primeira variavel
+rs = numero + 1#aqui vai ficar o resultado
+print("O sucessor do seu número é:",rs)#aqui é o codigo que mostrará o números sucessor
